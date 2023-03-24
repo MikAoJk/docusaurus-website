dfa5c74388295015ec7ca754046fa5cf2cec0836
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-03-24 08:01:43 +0100

    Set traings slash to false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,13 @@ const config = {
   url: 'https://mikaojk.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-website/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'MikAoJk', // Usually your GitHub org/user name.
   projectName: 'docusaurus-website', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
